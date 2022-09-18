879b523a1eb0ea3cbf08ffcb2c726ca922736d05
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-09-18 13:37:35 -0400

    Remove .js files

--- src/App.js
+++ src/App.js
@@ -1,61 +1,7 @@
-import Add from './AddField';
-import Item from './Item';
 import './App.css';
 
-import { useLocalStorage } from 'usehooks-ts';
-import { v4 as uuidv4 } from 'uuid';
-
 function App() {
-  const [items, setItems] = useLocalStorage('items', []);
-
-  function addHandler(text) {
-    setItems((currentItems) => [
-      ...currentItems,
-      { text, completed: false, id: uuidv4() },
-    ]);
-  }
-
-  function deleteHandler(id) {
-    setItems((currentItems) => currentItems.filter((el) => el.id !== id));
-  }
-
-  function toggleHandler(id) {
-    setItems((currentItems) =>
-      currentItems.map((el) => {
-        console.log(el);
-        if (el.id === id) {
-          return { ...el, completed: !el.completed };
-        }
-        return el;
-      })
-    );
-  }
-
-  return (
-    <div className="App">
-      <div className="top">
-        <h1 className="title">Hey there, Alex!</h1>
-        <div className="list">
-          {items.map((item, ind, _) => {
-            const text = item.text;
-            const completed = item.completed;
-            const id = item.id;
-            return (
-              <Item
-                deleteHandler={deleteHandler}
-                toggleHandler={toggleHandler}
-                key={id}
-                id={id}
-                text={text}
-                completed={completed}
-              />
-            );
-          })}
-        </div>
-      </div>
-      <Add addHandler={addHandler} />
-    </div>
-  );
+  <div></div>;
 }
 
 export default App;
