12b6623526d6a49ef62c8a1002bfc1c3417348f4
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-09-22 12:55:36 -0400

    Add click handler prop

--- src/Add/index.js
+++ src/Add/index.js
@@ -1,17 +1,10 @@
-// import styles
 import './index.css';
 
 import { useState } from 'react';
 
-function Add() {
-  // https://reactjs.org/docs/forms.html#:~:text=called%20%E2%80%9Ccontrolled%20components%E2%80%9D.-,Controlled%20Components,-In%20HTML%2C%20form
-  // This is a "controlled input"
-
-  // Use the `text` constant to work with the value of `text`
-  // and use the setText function to update the value of `text`
+function Add({ addHandler }) {
   const [text, setText] = useState();
 
-  // set any changes in the input to the value of `text`
   function changeHandler(event) {
     setText(event.target.value);
   }
@@ -24,7 +17,7 @@ function Add() {
         value={text}
         onChange={changeHandler}
       />
-      <button className="add-button">
+      <button className="add-button" onClick={() => addHandler(text)}>
         <img src="icons/add.svg" alt="Add" />
       </button>
     </div>
