7f625cd86c3b7542b88d8ce32b521d322e66c047
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-09-21 21:39:39 -0400

    Add Item component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-// import the component
+// import the components
+import Item from './Item';
 import Add from './Add';
 
 function App() {
   return (
     <div className="app">
+      <div className="top">
+        <h1 className="title">Hey there, Alex!</h1>
+        <div className="list">
+          <Item text="item" checked={false} id="1" />
+        </div>
+      </div>
       {/* use the component! */}
       <Add></Add>
     </div>
