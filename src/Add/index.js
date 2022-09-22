4f49d7e2ceb9159f9456b9440c60473b8df54b55
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-09-21 21:32:35 -0400

    Add comments

--- src/Add/index.js
+++ src/Add/index.js
@@ -1,10 +1,17 @@
+// import styles
 import './index.css';
 
 import { useState } from 'react';
 
 function Add({ addHandler }) {
+  // https://reactjs.org/docs/forms.html#:~:text=called%20%E2%80%9Ccontrolled%20components%E2%80%9D.-,Controlled%20Components,-In%20HTML%2C%20form
+  // This is a "controlled input"
+
+  // Use the `text` constant to work with the value of `text`
+  // and use the setText function to update the value of `text`
   const [text, setText] = useState();
 
+  // set any changes in the input to the value of `text`
   function changeHandler(event) {
     setText(event.target.value);
   }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+// import the component
 import Add from './Add';
 
 function App() {
   return (
     <div className="app">
+      {/* use the component! */}
       <Add></Add>
     </div>
   );
